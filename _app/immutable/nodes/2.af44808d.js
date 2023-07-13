aea9c02372d3486317fdb6c973e03070279e53cf
Alex Figliolia <alexfigliolia@gmail.com>
2023-07-13 12:24:13 -0700

    Updates

---
+++ _app/immutable/nodes/2.af44808d.js
@@ -0,0 +1,1 @@
+import{default as n}from"../chunks/_page.748dfeb9.js";const e=!0,o=Object.freeze(Object.defineProperty({__proto__:null,prerender:e},Symbol.toStringTag,{value:"Module"}));export{n as component,o as universal};
--- _app/immutable/nodes/2.f55e4c69.js
+++
@@ -1,1 +0,0 @@
-import{default as n}from"../chunks/_page.84c4e581.js";const e=!0,o=Object.freeze(Object.defineProperty({__proto__:null,prerender:e},Symbol.toStringTag,{value:"Module"}));export{n as component,o as universal};
